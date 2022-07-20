4e5c5339a88b7cba557195f1716b890e617dfa6e
treastrain / Tanaka Ryoga <tr@tret.jp>
2022-07-20 12:19:02 +0900

    お題002を追加

---
+++ SwiftUIWeeklyLayoutChallenge/Documentation.docc/Topics/Topic002.md
@@ -0,0 +1,14 @@
+# お題 002
+
+ナビゲーション構造のリスト
+
+- ナビゲーション階層による画面遷移を前提としてください（NavigationLink を用いてください）
+- macOS App は考慮しなくてよいこととします
+- エンティティ・サンプルデータ の提供があります（`Topic002View.swift` を参照、利用は必須ではありません）
+
+|iOS|tvOS|watchOS|
+|:-:|:-:|:-:|
+|![](Topic002_iOS)|![](Topic002_tvOS)|![](Topic002_watchOS)|
+
+## お題作成者
+treastrain ([@treastrain](https://twitter.com/treastrain))
---
+++ SwiftUIWeeklyLayoutChallenge/Topic002.swift
@@ -0,0 +1,48 @@
+//
+//  Topic002View.swift
+//  SwiftUIWeeklyLayoutChallenge
+//
+//  Created by treastrain on 2022/07/20.
+//
+
+import SwiftUI
+
+// MARK: - Entities
+fileprivate struct Vital: Identifiable {
+    let id = UUID()
+    let title: LocalizedStringKey
+    let value: Value
+    let date: Date
+    let iconSystemName: String
+    let color: Color
+    
+    enum Value {
+        case number(value: Double, style: NumberFormatter.Style, customUnit: String? = nil)
+        case dateComponents(_ dateComponents: DateComponents)
+        case measurement(value: Double, unit: Dimension, formattedUnit: Dimension? = nil)
+    }
+}
+
+// MARK: - Sample Data
+fileprivate let vitalData: [Vital] = [
+    .init(title: "取り込まれた酸素のレベル", value: .number(value: 0.99, style: .percent), date: Date(timeIntervalSinceNow: -300), iconSystemName: "o.circle.fill", color: .blue),
+    .init(title: "心拍数", value: .number(value: 61, style: .decimal, customUnit: "拍/分"), date: Date(timeIntervalSinceNow: -5400), iconSystemName: "heart.fill", color: .red),
+    .init(title: "睡眠", value: .dateComponents(.init(minute: 451)), date: Date(timeIntervalSinceNow: -87000), iconSystemName: "bed.double.fill", color: .green),
+    .init(title: "体温", value: .measurement(value: 36.4, unit: UnitTemperature.celsius), date: Date(timeIntervalSinceNow: -172800), iconSystemName: "thermometer", color: .red),
+]
+
+// MARK: - Views
+/// <doc:Topic002>
+public struct Topic002View: View {
+    public init() {}
+    
+    public var body: some View {
+        Text("Code your layout here!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Topic002View()
+    }
+}
