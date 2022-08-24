dd0112286ca67a350af9c7e9881589bb53d7c275
treastrain / Tanaka Ryoga <tr@tret.jp>
2022-08-24 18:28:14 +0900

    お題006のスクリーンショット生成時コードを追加

--- SwiftUIWeeklyLayoutChallenge/Topic006.swift
+++ SwiftUIWeeklyLayoutChallenge/Topic006.swift
@@ -27,10 +27,103 @@ public struct Topic006View: View {
 #if os(iOS)
 @available(iOS 15.0, *)
 fileprivate struct Topic006ContentView: View {
+    private let coordinateSpaceName = "stacks"
+    private var coordinateSpace: CoordinateSpace { .named(coordinateSpaceName) }
     private let paddingLength: CGFloat = 16
     
+    @State private var selectedTitle = ""
+    @State private var isPresented = false
+    @State private var position: CGPoint? = nil
+    
+    var drag: some Gesture {
+        DragGesture(coordinateSpace: coordinateSpace)
+            .onChanged { position = $0.location }
+    }
+    
     var body: some View {
-        Text("Code your layout here!")
+        ZStack {
+            Text("後ろに位置する View")
+            
+            VStack(alignment: .trailing) {
+                Spacer()
+                
+                HStack(alignment: .bottom) {
+                    Spacer()
+                    
+                    Menu(
+                        content: {
+                            Button(action: compose) {
+                                Label("新規作成", systemImage: "square.and.pencil")
+                            }
+                            Button(action: addImage) {
+                                Label("画像を追加", systemImage: "photo")
+                            }
+                            Button(action: addAudio) {
+                                Label("音声を追加", systemImage: "mic.badge.plus")
+                            }
+                        },
+                        label: {
+                            Image(systemName: "plus")
+                                .imageScale(.large)
+                                .frame(width: 50, height: 50)
+                                .foregroundColor(.white)
+                                .background(Color.accentColor)
+                                .clipShape(Circle())
+                        },
+                        primaryAction: compose
+                    )
+                    .padding(paddingLength)
+                    .ifLet(position) { view, position in
+                        view.position(position)
+                    }
+                    .gesture(drag)
+                    .background(
+                        GeometryReader { geometryProxy in
+                            Color.clear
+                                .onAppear {
+                                    if position == nil {
+                                        let frame = geometryProxy.frame(in: coordinateSpace)
+                                        position = .init(
+                                            x: frame.midX - (paddingLength / 2),
+                                            y: frame.midY - (paddingLength / 2)
+                                        )
+                                    }
+                                }
+                        }
+                    )
+                }
+            }
+            .coordinateSpace(name: coordinateSpaceName)
+        }
+        .alert(selectedTitle, isPresented: $isPresented) {
+            Button("閉じる") { isPresented = false }
+        }
+    }
+    
+    private func compose() {
+        selectedTitle = "新規作成"
+        isPresented = true
+    }
+    
+    private func addImage() {
+        selectedTitle = "画像を追加"
+        isPresented = true
+    }
+    
+    private func addAudio() {
+        selectedTitle = "音声を追加"
+        isPresented = true
+    }
+}
+
+extension View {
+    @ViewBuilder
+    fileprivate func `ifLet`<Wrapped, Content: View>(_ value: Optional<Wrapped>, transform: (Self, Wrapped) -> Content) -> some View {
+        if let value = value {
+            transform(self, value)
+        } else {
+            self
+        }
     }
 }
 #endif
